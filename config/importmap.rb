59c3b045766767529c20ed6529f0fd845e896115
Yaroslav Kibysh <yanet.prod@gmail.com>
2023-06-27 14:49:18 +0300

    Add leaflat as importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js"
+pin "leaflet-geosearch", to: "https://ga.jspm.io/npm:leaflet-geosearch@3.8.0/dist/geosearch.module.js"
