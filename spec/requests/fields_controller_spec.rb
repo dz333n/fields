4d801dd0fbb77a1b833ae7e0ca8820a171a95e88
Yaroslav Kibysh <yanet.prod@gmail.com>
2023-06-28 13:46:09 +0300

    Add update field tests

--- spec/requests/fields_controller_spec.rb
+++ spec/requests/fields_controller_spec.rb
@@ -11,7 +11,7 @@ def wrap_into_geojson_polygon(data)
   }
 end
 
-describe 'POST /fields', type: :request do
+describe '/fields', type: :request do
   let(:polygon_1) do
     [
       [
@@ -51,7 +51,7 @@ describe 'POST /fields', type: :request do
           -73.963257,
           40.558801
       ]
-   ]
+  ]
   end
 
   let(:result_multipolygon) do
@@ -78,17 +78,54 @@ describe 'POST /fields', type: :request do
 
   let(:precalculated_area) { 6208496762.301932 }
 
-  subject do
-    post '/fields', params: params.to_json, headers: { 'Content-Type': 'application/json' }
+  describe 'PUT /fields/:id' do
+    let(:field) { create(:field) }
+
+    subject do
+      put "/fields/#{field.id}", params: params.to_json, headers: { 'Content-Type': 'application/json' }
+    end
+
+    it 'changes field params' do
+      subject
+      updated_field = field.reload
+      expect(updated_field.name).to eq(name)
+      expect(updated_field.area).to eq(precalculated_area)
+      expect(RGeo::GeoJSON.encode(updated_field.shape)).to eq(result_multipolygon.with_indifferent_access)
+    end
+
+    context 'when polygons are not provided' do
+      let(:params) do
+        {
+          "polygons": [],
+          "name": name
+        }
+      end
+
+      it 'changes field params but keeps old map' do
+        subject
+        old_area = field.area
+        old_shape = field.shape
+        updated_field = field.reload
+        expect(updated_field.name).to eq(name)
+        expect(updated_field.area).to eq(old_area)
+        expect(RGeo::GeoJSON.encode(updated_field.shape)).to eq(RGeo::GeoJSON.encode(old_shape))
+      end
+    end
   end
 
-  it 'creates a new field with requested params' do
-    subject
-    response_json = JSON.parse(response.body)
-    new_field = Field.find(response_json['id'])
-    expect(new_field).to be_present
-    expect(new_field.name).to eq(name)
-    expect(new_field.area).to eq(precalculated_area)
-    expect(RGeo::GeoJSON.encode(new_field.shape)).to eq(result_multipolygon.with_indifferent_access)
+  describe 'POST /fields' do
+    subject do
+      post '/fields', params: params.to_json, headers: { 'Content-Type': 'application/json' }
+    end
+
+    it 'creates a new field with requested params' do
+      subject
+      response_json = JSON.parse(response.body)
+      new_field = Field.find(response_json['id'])
+      expect(new_field).to be_present
+      expect(new_field.name).to eq(name)
+      expect(new_field.area).to eq(precalculated_area)
+      expect(RGeo::GeoJSON.encode(new_field.shape)).to eq(result_multipolygon.with_indifferent_access)
+    end
   end
 end
